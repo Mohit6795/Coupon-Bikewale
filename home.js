c48c661d1f1f60be00bbc1c6a9402102ae3c950e
Mohit6795 <mohit.10saxena@gmail.com>
2022-08-26 00:32:59 +0530

    First commit

---
+++ home.js
@@ -0,0 +1,17 @@
+
+// const changeMode=()=>{
+//     let mybody = document.body;
+//     mybody.classList.toggle('mydark')
+    
+// }   
+
+// coupon js
+function loadCoupon(){
+    document.getElementById('coupon').style.display="block";
+    document.getElementById('content').style.opacity="0.5";
+}
+const closeCoupon(){
+    document.getElementById('coupon').style.display="none";
+    document.getElementById('content').style.opacity="1";
+}
+
